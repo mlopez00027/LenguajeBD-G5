89ac1b55546c5f488fd14c5c175854de861e5327
maricruz <mlopez00027@ufide.ac.cr>
2024-07-23 14:23:47 -0600

    conexion a la base de datos
    conexion a BD con mi usuario: PROYECTO_MARICRUZ

---
+++ Model/conexionBD.php
@@ -0,0 +1,29 @@
+<?php
+
+function AbrirBaseDatos()
+{
+
+    $host="localhost";
+    $port="1521";
+    $dbname="orcl";
+    $user="PROYECTO_MARICRUZ";
+    $passwrod="12345";
+
+    $dsn = "oci:dbname=(DESCRIPTION=(ADDRESS_LIST=(ADDRESS=(PROTOCOL=TCP)(HOST=$host)(PORT=$port)))(CONNECT_DATA=(SID=$dbname)))";
+
+    try{
+        $conexion=new PDO($dsn, $user, $passwrod);
+        $conexion->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        echo "Conexión exitosa";
+    }catch (PDOException $e){
+        echo "No se estableció la conexión: ".$e->getMessage();
+    }
+        return $conexion;
+    }
+
+function CerrarBaseDatos($conexion)
+{
+    $conexion = null;
+}
+
+
--- index.php
+++ index.php
@@ -1,3 +1,12 @@
-<?php   
-    header("location: View/home.php");
+<?php include 'Model/conexionBD.php';
+
+$conexion = AbrirBaseDatos();
+if ($conexion) {
+} 
+else {
+    echo "Error al conectar a la base de datos.";
+}
+
+header("location: View/home.php");
+
 ?>
