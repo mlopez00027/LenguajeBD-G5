7413e194efd21b09426af721d08f060ee918aa84
Parriux <jparra11332@ufide.ac.cr>
2024-07-26 15:41:09 -0600

    Revert Registrar

--- Controller/usuarioController.php
+++ Controller/usuarioController.php
@@ -1,16 +1,8 @@
 <?php
 include_once '../Model/usuarioModel.php';
 
-if (isset($_POST["btnRegistrarme"])) {
-    $nombre = $_POST['nombre'];
-    $email = $_POST['email'];
-    $password = $_POST['password'];
-
-    if (RegistrarUsuario($conn, $nombre, $email, $password)) {
-        header("Location: ../View/login.php");
-        exit();
-    } else {
-        echo "Error al registrar el usuario.";
-    }
+if(isset($_POST["btnRegistrarme"]))
+{
+    RegistrarUsuario($conn, $nombre, $email, $password);
+    header("location: ../View/login.php");
 }
-
